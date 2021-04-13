dcc8f820119b775822b7b67f1197bd50aacccb25
Otavio Pellicano <otavio.pellicano@gmail.com>
2021-04-13 20:50:26 -0300

    Remove catch2 from deps folder and use conda pkg

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch2/catch_all.hpp"
+
+#include <catch2/catch.hpp>
 
 #include <iostream>
 #include <vector>
@@ -31,7 +32,7 @@ TEST_CASE("Invert first input", "[Not]") {
 	
 	for (int row = 0; row < training_data_output.rows(); row++){
 		
-		REQUIRE(training_data_output(row, 0) - expected_output(row, 0) == Catch::Approx(0).margin(.1));
+        REQUIRE(training_data_output(row, 0) - expected_output(row, 0) == ::Approx(0).margin(.1));
 	}
 
 	Eigen::MatrixXd dataset2(4, 3);
@@ -47,7 +48,7 @@ TEST_CASE("Invert first input", "[Not]") {
 
 	auto test_data_output = nn.feed_forward(dataset2);
 	for (int row = 0; row < test_data_output.rows(); row++) {
-		REQUIRE(test_data_output(row, 0) - expected_test_data(row, 0) == Catch::Approx(0.0).margin(0.1));
+        REQUIRE(test_data_output(row, 0) - expected_test_data(row, 0) == ::Approx(0.0).margin(0.1));
 	}
 
 }
