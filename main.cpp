3f2a9929a81a9ec7a023a633df8948b57c8e398d
Adrianno Sampaio <adriannosampaio@gmail.com>
2021-05-15 14:19:35 -0300

    Empty main function

--- main.cpp
+++ main.cpp
@@ -3,34 +3,12 @@
 #include <iostream>
 #include <vector>
 #include <Dense>
-#include <NeuralNet.hpp>
-
+#include "NeuralNet.hpp"
+#include "ActivationFunction.hpp"
 
 int main()
 {
-	Eigen::MatrixXd dataset(6, 3);
-	dataset <<  
-		0, 0, 0, 
-		0, 0, 1, 
-		0, 1, 1, 
-		1, 0, 1, 
-		1, 1, 0, 
-		1, 1, 1;
 
-	Eigen::MatrixXd output(6, 1);
-	output << 0, 1, 1, 1, 1, 1;
-
-	auto nn = NeuralNetwork(3, {3, 3, 1});
-	nn.train(dataset, output);
-	std::cout << "test result: " << nn.feed_forward(dataset) << "\n";
-
-	Eigen::MatrixXd dataset2(4, 3);
-	dataset2 << 
-		0, 0, 0, 
-		0, 1, 1,
-		1, 0, 0,
-		1, 1, 1;
-	std::cout << "test result 2: " << nn.feed_forward(dataset2) << "\n";
 
 	return 0;
 }
